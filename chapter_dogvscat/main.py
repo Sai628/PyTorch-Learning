2dba7a6dd0f8b1149fc5bfd33dd24cd1e08f3f4d
Sai628 <sai100628@gmail.com>
2018-08-30 17:28:22 +0800

    fix bug & optimize progress

--- chapter_dogvscat/main.py
+++ chapter_dogvscat/main.py
@@ -44,11 +44,11 @@ def train(**kwargs):
     previous_loss = 1e100
 
     # 开始训练
-    for epoch in range(opt.max_epoch):
+    for epoch in tqdm(range(opt.max_epoch), desc='epoch', unit='epoch'):
         loss_meter.reset()
         confusion_matrix.reset()
 
-        for i, (data, label) in tqdm(enumerate(train_dataloader)):
+        for i, (data, label) in tqdm(enumerate(train_dataloader), desc='train', unit='batch'):
             # 训练模型参数
             input = V(data)
             target = V(label)
@@ -66,29 +66,29 @@ def train(**kwargs):
             confusion_matrix.add(score.data, target.data)
 
             if i % opt.print_freq == opt.print_freq - 1:
-                vis.plot('loss', loss_meter.value().item())
+                vis.plot('loss', loss_meter.value()[0])
 
                 # 如果需要的话, 进入debug模式
                 if os.path.exists(opt.debug_file):
                     import ipdb
                     ipdb.set_trace()
 
-    model.save()
+        model.save()
 
-    # 计算验证集上的指标以及可视化
-    val_cm, val_accuracy = val(model, val_dataloader)
-    vis.plot('val_accuracy', val_accuracy)
-    vis.log("epoch:{epoch}, lr:{lr}, loss:{loss}, train_cm:{train_cm}, val_cm:{val_cm}"
-            .format(epoch=epoch, lr=lr, loss=loss_meter.value().item()),
-                    train_cm=str(confusion_matrix.value()), val_cm=str(val_cm.value()))
+        # 计算验证集上的指标以及可视化
+        val_cm, val_accuracy = val(model, val_dataloader)
+        vis.plot('val_accuracy', val_accuracy)
+        vis.log("epoch:{epoch}, lr:{lr}, loss:{loss}, train_cm:{train_cm}, val_cm:{val_cm}"
+                .format(epoch=epoch, lr=lr, loss=loss_meter.value()[0],
+                        train_cm=str(confusion_matrix.value()), val_cm=str(val_cm.value())))
 
-    # 如果损失不再下降, 则降低学习率
-    if loss_meter.value().item() > previous_loss:
-        lr = lr * opt.lr_decay
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+        # 如果损失不再下降, 则降低学习率
+        if loss_meter.value()[0] > previous_loss:
+            lr = lr * opt.lr_decay
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
 
-    previous_loss = loss_meter.value().item()
+        previous_loss = loss_meter.value()[0]
 
 
 def val(model, dataloader):
@@ -100,9 +100,10 @@ def val(model, dataloader):
     model.eval()
 
     confusion_matrix = meter.ConfusionMeter(2)
-    for i, data in tqdm(enumerate(dataloader)):
+    for i, data in tqdm(enumerate(dataloader), desc='val', unit='batch'):
         input, label = data
-        val_input = V(input, volatile=True)
+        with t.no_grad():
+            val_input = V(input)
         if opt.use_gpu:
             val_input = val_input.cuda()
         score = model(val_input)
@@ -132,8 +133,9 @@ def test(**kwargs):
 
     # 开始测试
     results = []
-    for i, (data, path) in tqdm(enumerate(test_dataloader)):
-        input = V(data, volatile=True)
+    for i, (data, path) in tqdm(enumerate(test_dataloader), desc='test'):
+        with t.no_grad():
+            input = V(data)
         if opt.use_gpu:
             input = input.cuda()
         score = model(input)
